4b29ca0fb93b4cb7dea033abb41c182406de3edc
Rusty Divine <rusty.osmyn@gmail.com>
2024-08-05 16:01:59 -0500

    Comments for clarity

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,7 @@
 import { MetadataRoute } from "next";
 
+// https://nextjs.org/docs/app/api-reference/file-conventions/metadata/robots
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,11 +1,13 @@
 import { MetadataRoute } from "next";
 
+// https://nextjs.org/docs/app/api-reference/file-conventions/metadata/sitemap
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://wheresthefruit.com",
       lastModified: new Date(),
-      changeFrequency: "yearly",
+      changeFrequency: "yearly", // always, hourly, daily, weekly, monthly, yearly, never
       priority: 1,
     },
   ];
