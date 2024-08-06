74177c857769813d7bd27e3283a627e06f4232b1
Rusty Divine <rusty.osmyn@gmail.com>
2024-08-06 13:39:09 -0500

    hamburger menu working

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
@@ -6,19 +7,29 @@ import clsx from "clsx";
 import LinkButton from "./LinkButton";
 
 export default function Navigation() {
+  const [isOpen, setIsOpen] = useState(false);
   const pathname = usePathname();
+
   const links = [
     { name: "Home", href: "/" },
     { name: "Fruits", href: "/fruits" },
     { name: "About", href: "/about" },
   ];
+
+  const handleBurgerClick = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
-    <nav className="flex mb-24 justify-between items-center py-6 px-10 relative">
-      <Link href="/" className="text-lg mr-10 2xl:mr-16">
+    <nav className="flex mb-24 justify-between items-center py-6 px-10 relative mr-7 z-10">
+      <Link href="/" className="text-lg mr-10 2lg:mr-16">
         <Image src="/logo.png" alt="Fruit Finder Logo" width="50" height="50" />
       </Link>
-      <div className="xl:hidden">
-        <button className="focus:outline-none text-indigo-900 hover:text-indigo-800">
+      <div className="lg:hidden">
+        <button
+          className="navbar-burger flex items-center text-blue-600 p-3"
+          onClick={handleBurgerClick}
+        >
           <svg
             className="block h-6 w-6"
             fill="currentColor"
@@ -29,13 +40,41 @@ export default function Navigation() {
             <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
           </svg>
         </button>
+        <div
+          className={clsx("w-5 h-5 flex justify-around flex-col", {
+            hidden: !isOpen,
+          })}
+        >
+          <ul>
+            {links.map((link) => (
+              <li key={link.href}>
+                <Link
+                  href={link.href}
+                  className={clsx("text-lg 2xl:mr-16 hover:underline", {
+                    underline: pathname === link.href,
+                  })}
+                >
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+            <li>
+              <LinkButton
+                href="/signup"
+                className="text-lg mr-200 2xl:mr-16 inline-block text-nowrap"
+              >
+                Sign Up
+              </LinkButton>
+            </li>
+          </ul>
+        </div>
       </div>
-      <ul className="hidden xl:flex absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <ul className="hidden lg:flex absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         {links.map((link) => (
           <li key={link.href}>
             <Link
               href={link.href}
-              className={clsx("text-lg mr-10 2xl:mr-16 hover:underline", {
+              className={clsx("text-lg mr-10 2lg:mr-16 hover:underline", {
                 underline: pathname === link.href,
               })}
             >
@@ -44,8 +83,8 @@ export default function Navigation() {
           </li>
         ))}
       </ul>
-      <div className="hidden xl:flex items-center">
-        <LinkButton href="/signup" className="text-lg mr-10 2xl:mr-16">
+      <div className="hidden lg:flex items-center">
+        <LinkButton href="/signup" className="text-lg mr-10 2lg:mr-16">
           Sign Up
         </LinkButton>
       </div>
