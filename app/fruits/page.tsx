3e44ebd64e297ef02edeff90526749c62de4aa06
Rusty Divine <rusty.osmyn@gmail.com>
2024-08-15 11:36:15 -0500

    Get API working

--- app/fruits/page.tsx
+++ app/fruits/page.tsx
@@ -5,15 +5,19 @@ export const metadata: Metadata = {
   title: "All Fruits",
 };
 
-const FruitsPage: React.FC = () => {
+export default async function FruitsPage() {
+  const base = process.env.API_BASE_URL;
+  const url = `${base}/accounts`;
+  const response = await fetch(url);
+  const data = await response.json();
+
   return (
     <>
       <Header />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1>Fruits Page</h1>
+        <p>{JSON.stringify(data)}</p>
       </main>
     </>
   );
-};
-
-export default FruitsPage;
+}
